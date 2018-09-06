8a6fdeab0deb498487fa11739423b42b8e183cde
Morithule <morithule@gmail.com>
2018-09-06 12:07:55 -0400

    Initial project setup

---
+++ BabySitterPayCalculator/BabysitterPayCalculatorTest/BabySitterPayCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterPayCalculator;
+
+namespace BabysitterPayCalculatorTest
+{
+    [TestClass]
+    public class BabysitterPayCalculatorTests
+    {
+        private Babysitter Calculator;
+        
+        [TestInitialize]
+        public void InitTests()
+        {
+            Calculator = new Babysitter();
+        }
+
+        [TestMethod]
+        public void TestTheTestPass()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestTheTestFail()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void TestInit()
+        {
+            Assert.IsTrue(Calculator.GetType().Equals(typeof(Babysitter)));
+        }
+    }
+}
---
+++ BabySitterPayCalculator/BabysitterPayCalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BabySitterPayCalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BabySitterPayCalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50316e82-44e6-4b0a-91e2-b8fca3fa5479")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
